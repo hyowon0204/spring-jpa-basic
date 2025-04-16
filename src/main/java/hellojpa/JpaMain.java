c910423e8b0d2f167f69a100fcf9a2f60fc671a8
hyowon <ally0204@naver.com>
2025-04-16 13:25:20 +0900

    [FEAT/#3] 트랜잭션을 지원하는 쓰기 지연

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -15,11 +15,12 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //비영속
-            Member findMember1 = em.find(Member.class, 101L);
-            Member findMember2 = em.find(Member.class, 101L);
+            Member member1 = new Member(110L,"A");
+            Member member2 = new Member(120L,"B");
 
-            System.out.println("result =" +(findMember1==findMember2));
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("============");
 
             tx.commit();
         }catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,10 +5,16 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Member {
-
     @Id
     private Long id;
     private String name;
+
+    public Member() {}
+
+    public Member(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
     public Long getId() {
         return id;
